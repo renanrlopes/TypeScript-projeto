1775f423192cc561b062061d09bc33ff43140ea3
Renan Lopes <rrloppes7@gmail.com>
2025-12-16 11:17:50 -0300

    alterando tsconfig

--- build/index.js
+++ build/index.js
@@ -1,9 +1,8 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const input1 = document.getElementById('num1');
-const input2 = document.getElementById('num2');
-const buttonSoma = document.getElementById('buttonSomar');
-const buttonSubtrair = document.getElementById('buttonSubtrair');
+var input1 = document.getElementById('num1');
+var input2 = document.getElementById('num2');
+var buttonSoma = document.getElementById('buttonSomar');
+var buttonSubtrair = document.getElementById('buttonSubtrair');
 function somar(a, b) {
     return a + b;
 }
@@ -11,10 +10,10 @@ function subtrair(a, b) {
     return a - b;
 }
 buttonSoma.addEventListener('click', function () {
-    const resultado = somar(Number(input1.value), Number(input2.value));
+    var resultado = somar(Number(input1.value), Number(input2.value));
     console.log(resultado);
 });
 buttonSubtrair.addEventListener('click', function () {
-    const resultado = subtrair(Number(input1.value), Number(input2.value));
+    var resultado = subtrair(Number(input1.value), Number(input2.value));
     console.log(resultado);
 });
